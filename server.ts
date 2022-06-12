fef4ee3d710a29f9387e816010726bf9be614c23
vgrachev <victorgrachev@mail.ru>
2022-06-12 20:34:15 +0700

    Создание сервера статики

---
+++ server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+// @ts-ignore
+import favicon from 'express-favicon';
+import path from 'path';
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.png'));
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port);
